c1831aaebce6fc966751fd2130474814c497c4c1
Art Goldhammer <art.goldhammer@gmail.com>
2024-08-20 16:32:39 -0400

    moved hostdata into separate module

---
+++ src/hostdata.rs
@@ -0,0 +1,27 @@
+use crate::geo;
+use console::style;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HostData {
+    pub ip: String,
+    pub geodata: geo::Geodata,
+    pub ptr_records: Vec<String>,
+}
+
+impl fmt::Display for HostData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(
+            f,
+            "{}: {}",
+            style("IP").bold().red(),
+            style(&self.ip).green()
+        )?;
+
+        write!(f, "{}", self.geodata)?;
+        self.ptr_records.iter().try_for_each(|record| {
+            writeln!(f, "{}: {}", style("host").red(), style(record).green())
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,16 @@
 use anyhow::Result;
-use console::style;
 use futures::stream::TryStreamExt;
 use mongodb::bson::doc;
 use mongodb::options::IndexOptions;
 use mongodb::{Client, Collection, Database, IndexModel};
-// use regex::Regex;
-use serde::{Deserialize, Serialize};
-use std::fmt;
 
 pub mod geo;
+
+pub(crate) mod hostdata;
 pub mod logentries;
 
+use hostdata::HostData;
 use logentries::LogEntry;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct HostData {
-    pub ip: String,
-    pub geodata: geo::Geodata,
-    pub ptr_records: Vec<String>,
-}
-
-impl fmt::Display for HostData {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(
-            f,
-            "{}: {}",
-            style("IP").bold().red(),
-            style(&self.ip).green()
-        )?;
-
-        write!(f, "{}", self.geodata)?;
-        self.ptr_records.iter().try_for_each(|record| {
-            writeln!(f, "{}: {}", style("host").red(), style(record).green())
-        })
-    }
-}
 
 async fn get_db(dbname: &str) -> Result<Database> {
     let client = Client::with_uri_str("mongodb://192.168.0.128:27017").await?;
